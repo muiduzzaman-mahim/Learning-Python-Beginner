43484967b83b5a3e168e765440c111d7c2f2cb35
Muiduzzaman Mahim <47756767+muiduzzaman-mahim@users.noreply.github.com>
2020-10-11 16:25:17 +0600

    Add files via upload

---
+++ Exam Grading System.py
@@ -0,0 +1,36 @@
+#start
+
+print('Enter The exam mark')
+a =float(input())
+
+if a>79:
+    print('Congratulation. You have got "A+". You got the highest grade.')
+else:
+    if a>74:
+        print('Congratulation.You have got "A" and you pass the exam. ')
+    else:
+        if a>69:
+            print('Congratulation.You have got "A-" and you pass the exam.')
+        else:
+            if a>64:
+                print('Congratulation.You have got "B+" and you pass the exam.')
+            else:
+                if a>59:
+                    print('Congratulation.You have got "B" and you pass the exam.')
+                else:
+                    if a>54:
+                        print('Congratulation.You have got "B-" and you pass the exam.')
+                    else:
+                        if a>49:
+                            print('Congratulation.You have got "C" and you pass the exam.')
+                        else:
+                            if a>45:
+                                print('Congratulation.You have got "D" and you pass the exam.')
+                            else:
+                                if a>39:
+                                    print('Congratulation.You have got "E" and you pass the exam.')
+                                else:
+                                    print('Do not worry! You have got "F" and Please take a better preparation again.')
+print('Thank You.....!')
+
+#end
