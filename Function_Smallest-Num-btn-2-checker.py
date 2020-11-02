86987565be91de4ab28301119877431546f09ee8
Muiduzzaman Mahim <47756767+muiduzzaman-mahim@users.noreply.github.com>
2020-11-02 15:49:51 +0600

    HW 2 Nov 2020

---
+++ Function_Smallest-Num-btn-2-checker.py
@@ -0,0 +1,11 @@
+def largest_num(num1,num2):
+    if num1 < num2:
+        print('{} is the smallest number'.format(num1))
+    else:
+        print('{} is the smallest number'.format(num2))
+
+print('Enter two numbers to check which is smallest:\n')
+num1 = int(input())
+num2 = int(input())
+
+largest_num(num1, num2)
