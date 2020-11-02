1cf67b25a4ec420a262dfbf2725944261d456c29
Muiduzzaman Mahim <47756767+muiduzzaman-mahim@users.noreply.github.com>
2020-11-02 13:35:04 +0600

    create class November 2020

---
+++ Class-01-Nov-2020.py
@@ -0,0 +1,18 @@
+#function
+
+# num = 10 
+# if num%2 == 0:
+#     print('Even Number')
+# else:
+#     print('Odd Number')
+
+
+def number_even_odd(num):
+    if num%2 == 0:
+        print('Even Number')
+    else:
+        print('Odd Number')
+num = int(input())
+number_even_odd(num)
+
+
